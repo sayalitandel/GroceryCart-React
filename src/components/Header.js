ea8f38fc519f3904f1823f7412b245cc88cc7793
Sayali Tandel <st31610n@pace.edu>
2022-06-09 19:11:29 -0400

    css added and applied to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function Header(props) {
-    return <div>Header</div>;
+    return (
+        <header className='row block center'>
+            <div>
+                <a href="#/">
+                    <h1>Grocery Shop</h1>
+                </a>
+            </div>
+            <div>
+                <a href = "#/cart">Cart</a> <a href = "#/sign-in">Sign in</a>
+            </div>
+        </header>
+
+    );
 }
