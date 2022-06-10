73710f7b86c6465dc78006e841e9ed252308a9c0
Sayali Tandel <st31610n@pace.edu>
2022-06-10 11:12:36 -0400

    added another image and data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import logo from '../image-logo.png';
 
 export default function Header(props) {
     const { countCartItems } = props;
@@ -6,18 +7,21 @@ export default function Header(props) {
         <header className='row block center'>
             <div>
                 <a href="#/">
+                    {/* <img src={logo} height={100} width={100} alt='Logo'></img> */}
                     <h1>Grocery Shop</h1>
                 </a>
             </div>
-            <div>
-                <a href="#/cart">Cart {''}
-                    {countCartItems ? (
-                        <button className='badge'>{countCartItems}</button>
-                    ) : (
-                        ''
-                    )}
-                </a>
-                <a href="#/sign-in">Sign in</a>
+            <div className='theme'>
+                <strong>
+                    <a href="#/cart">Cart {''}
+                        {countCartItems ? (
+                            <button className='badge'>{countCartItems}</button>
+                        ) : (
+                            ''
+                        )}
+                    </a>
+                    <a href="#/sign-in">Sign in</a>
+                </strong>
             </div>
         </header>
 
--- src/data.js
+++ src/data.js
@@ -4,19 +4,25 @@ const data = {
             id: '1',
             name: 'Milk',
             price: 2.99,
-            image: 'https://picsum.photos/id/237/200/300'
+            image: 'https://drive.google.com/uc?export=view&id=1F4bszqyGr5ybuOAQXLPG8L7EeFl2GEml'
         },
         {
             id: '2',
             name: 'Cookies',
-            price: 2.99,
-            image: 'https://picsum.photos/id/237/200/300'
+            price: 4.99,
+            image: 'https://drive.google.com/uc?export=view&id=1_MfoWH40y4M1supZDJO2o-7hfMZrUikD'
         },
         {
             id: '3',
             name: 'Pasta',
-            price: 2.99,
-            image: 'https://picsum.photos/id/237/200/300'
+            price: 3.49,
+            image: 'https://drive.google.com/uc?export=view&id=1Ir2U83_K_FOzHiC1n7LiL4MV1TnAOAoE'
+        },
+        {
+            id: '4',
+            name: 'Kellogs',
+            price: 6.99,
+            image: 'https://drive.google.com/uc?export=view&id=15wtFi8yyGL17_G07cVCiLI7QVfdMncRt'
         },
     ],
 };
